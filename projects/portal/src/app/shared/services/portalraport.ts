e65e7e04616749f4bb408ea9554a29c689c148e8
Sławomir Simla <slawiks@wp.pl>
2019-11-29 19:03:44 +0100

    portal class

---
+++ projects/portal/src/app/shared/services/portalraport.ts
@@ -0,0 +1,12 @@
+
+export class Portalraport {
+	"id_raportu_portal": number;
+	"nazwa": string;
+	"opis": string;
+	"url": string;
+	"id_dokumentacji": number;
+	"logowanie": number;
+	"jednostki_uprawnienia": number;
+	"data_uruchomienia": string;
+	"id_wniosku": string;
+}
